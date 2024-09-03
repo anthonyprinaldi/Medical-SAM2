26333831259c20e598dc79dbbb53631c334a8b4a
Anthony Rinaldi <anthony.rinaldi04@gmail.com>
2024-09-03 15:16:50 -0400

    [update] Format and fix syntax

--- func_3d/dataset/neurosam.py
+++ func_3d/dataset/neurosam.py
@@ -2,24 +2,39 @@
     Anthony Rinaldi
 """
 
-import os
+import argparse
+from pathlib import Path
+from typing import Optional
 
 import numpy as np
 import torch
 from PIL import Image
 from torch.utils.data import Dataset
+from torchvision.transforms import Compose
 
 from func_3d.utils import generate_bbox, random_click
 
 
 class NEUROSAM(Dataset):
-    def __init__(self, args, data_path , transform = None, transform_msk = None, mode = 'Training',prompt = 'click', seed=None, variation=0):
+    def __init__(
+        self,
+        args: argparse.Namespace,
+        data_path: str,
+        transform: Optional[Compose]=None,
+        transform_msk: Optional[Compose]=None,
+        mode: str="Training",
+        prompt: str="click",
+        seed: Optional[int]=None,
+        variation: int=0,
+    ):
 
         # Set the data list for training
-        self.name_list = os.listdir(os.path.join(data_path, mode, 'image'))
-        
+        self.name_list = [
+            x for x in (Path(data_path) / mode / "image").iterdir() if x.is_dir()
+        ]
+
         # Set the basic information of the dataset
-        self.data_path = data_path
+        self.data_path = Path(data_path)
         self.mode = mode
         self.prompt = prompt
         self.img_size = args.image_size
@@ -27,7 +42,7 @@ class NEUROSAM(Dataset):
         self.transform_msk = transform_msk
         self.seed = seed
         self.variation = variation
-        if mode == 'Training':
+        if mode == "Training":
             self.video_length = args.video_length
         else:
             self.video_length = None
@@ -41,31 +56,40 @@ class NEUROSAM(Dataset):
 
         """Get the images"""
         name = self.name_list[index]
-        img_path = os.path.join(self.data_path, self.mode, 'image', name)
-        mask_path = os.path.join(self.data_path, self.mode, 'mask', name)
-        data_seg_3d_shape = np.load(mask_path + '/0.npy').shape
-        num_frame = len(os.listdir(mask_path))
+        img_path: Path = self.data_path / self.mode / "image" / name
+        mask_path: Path = self.data_path / self.mode / "mask" / name
+        data_seg_3d_shape = np.load(mask_path / "0.npy").shape
+        num_frame = len(mask_path.iterdir())
         data_seg_3d = np.zeros(data_seg_3d_shape + (num_frame,))
+        
         for i in range(num_frame):
-            data_seg_3d[..., i] = np.load(os.path.join(mask_path, f'{i}.npy'))
+            data_seg_3d[..., i] = np.load(mask_path / f"{i}.npy")
+        
+        # only include slices with GT
         for i in range(data_seg_3d.shape[-1]):
             if np.sum(data_seg_3d[..., i]) > 0:
                 data_seg_3d = data_seg_3d[..., i:]
                 break
+        
         starting_frame_nonzero = i
+
         for j in reversed(range(data_seg_3d.shape[-1])):
             if np.sum(data_seg_3d[..., j]) > 0:
-                data_seg_3d = data_seg_3d[..., :j+1]
+                data_seg_3d = data_seg_3d[..., : j + 1]
                 break
+        
         num_frame = data_seg_3d.shape[-1]
+
         if self.video_length is None:
             video_length = int(num_frame / 4)
         else:
             video_length = self.video_length
-        if num_frame > video_length and self.mode == 'Training':
+        
+        if num_frame > video_length and self.mode == "Training":
             starting_frame = np.random.randint(0, num_frame - video_length + 1)
         else:
             starting_frame = 0
+        
         img_tensor = torch.zeros(video_length, 3, self.img_size, self.img_size)
         mask_dict = {}
         point_label_dict = {}
@@ -73,61 +97,71 @@ class NEUROSAM(Dataset):
         bbox_dict = {}
 
         for frame_index in range(starting_frame, starting_frame + video_length):
-            img = Image.open(os.path.join(img_path, f'{frame_index + starting_frame_nonzero}.jpg')).convert('RGB')
+            img = Image.open(
+                img_path / f"{frame_index + starting_frame_nonzero}.jpg"
+            ).convert("RGB")
             mask = data_seg_3d[..., frame_index]
             # mask = np.rot90(mask)
             obj_list = np.unique(mask[mask > 0])
             diff_obj_mask_dict = {}
-            if self.prompt == 'bbox':
+            if self.prompt == "bbox":
                 diff_obj_bbox_dict = {}
-            elif self.prompt == 'click':
+            elif self.prompt == "click":
                 diff_obj_pt_dict = {}
                 diff_obj_point_label_dict = {}
             else:
-                raise ValueError('Prompt not recognized')
+                raise ValueError("Prompt not recognized")
             for obj in obj_list:
                 obj_mask = mask == obj
                 # if self.transform_msk:
                 obj_mask = Image.fromarray(obj_mask)
                 obj_mask = obj_mask.resize(newsize)
                 obj_mask = torch.tensor(np.array(obj_mask)).unsqueeze(0).int()
-                    # obj_mask = self.transform_msk(obj_mask).int()
+                # obj_mask = self.transform_msk(obj_mask).int()
                 diff_obj_mask_dict[obj] = obj_mask
 
-                if self.prompt == 'click':
-                    diff_obj_point_label_dict[obj], diff_obj_pt_dict[obj] = random_click(np.array(obj_mask.squeeze(0)), point_label, seed=None)
-                if self.prompt == 'bbox':
-                    diff_obj_bbox_dict[obj] = generate_bbox(np.array(obj_mask.squeeze(0)), variation=self.variation, seed=self.seed)
+                if self.prompt == "click":
+                    diff_obj_point_label_dict[obj], diff_obj_pt_dict[obj] = (
+                        random_click(
+                            np.array(obj_mask.squeeze(0)), point_label, seed=None
+                        )
+                    )
+                if self.prompt == "bbox":
+                    diff_obj_bbox_dict[obj] = generate_bbox(
+                        np.array(obj_mask.squeeze(0)),
+                        variation=self.variation,
+                        seed=self.seed,
+                    )
             # if self.transform:
-                # state = torch.get_rng_state()
-                # img = self.transform(img)
-                # torch.set_rng_state(state)
+            # state = torch.get_rng_state()
+            # img = self.transform(img)
+            # torch.set_rng_state(state)
             img = img.resize(newsize)
             img = torch.tensor(np.array(img)).permute(2, 0, 1)
 
             img_tensor[frame_index - starting_frame, :, :, :] = img
             mask_dict[frame_index - starting_frame] = diff_obj_mask_dict
-            if self.prompt == 'bbox':
+            if self.prompt == "bbox":
                 bbox_dict[frame_index - starting_frame] = diff_obj_bbox_dict
-            elif self.prompt == 'click':
+            elif self.prompt == "click":
                 pt_dict[frame_index - starting_frame] = diff_obj_pt_dict
-                point_label_dict[frame_index - starting_frame] = diff_obj_point_label_dict
+                point_label_dict[frame_index - starting_frame] = (
+                    diff_obj_point_label_dict
+                )
 
-
-        image_meta_dict = {'filename_or_obj':name}
-        if self.prompt == 'bbox':
+        image_meta_dict = {"filename_or_obj": name}
+        if self.prompt == "bbox":
             return {
-                'image':img_tensor,
-                'label': mask_dict,
-                'bbox': bbox_dict,
-                'image_meta_dict':image_meta_dict,
+                "image": img_tensor,
+                "label": mask_dict,
+                "bbox": bbox_dict,
+                "image_meta_dict": image_meta_dict,
             }
-        elif self.prompt == 'click':
+        elif self.prompt == "click":
             return {
-                'image':img_tensor,
-                'label': mask_dict,
-                'p_label':point_label_dict,
-                'pt':pt_dict,
-                'image_meta_dict':image_meta_dict,
+                "image": img_tensor,
+                "label": mask_dict,
+                "p_label": point_label_dict,
+                "pt": pt_dict,
+                "image_meta_dict": image_meta_dict,
             }
-    
