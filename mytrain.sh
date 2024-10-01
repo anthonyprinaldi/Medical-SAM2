c3ee19f5fe99e482cc0e6618102067516ab16035
Anthony Rinaldi <anthony.rinaldi04@gmail.com>
2024-10-01 16:01:24 -0400

    [feat] Update SLURM training script

--- mytrain.sh
+++ mytrain.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --account=rrg-mgoubran
-#SBATCH --job-name=train-medsam-slice
+#SBATCH --job-name=train-medsam-slice-official
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=100GB
-#SBATCH --time=00-00:20:00
-#SBATCH --gres=gpu:1
-#SBATCH --output=train-medsam-slice-%j.out
+#SBATCH --time=06-00:00:00
+#SBATCH --gres=gpu:2
+#SBATCH --output=train-medsam-slice-2gpu%j.out
 
 
 module load scipy-stack opencv cuda
@@ -18,17 +18,17 @@ which python
 pwd
 
 python -u train_3d.py \
-    -exp_name medsam-slice \
+    -exp_name medsam-slice-192img-16video \
     -vis True \
     -pretrain "checkpoints/MedSAM2_pretrain.pth" \
     -val_freq 1 \
     -gpu True \
     -gpu_device 0 \
-    -image_size 512 \
-    -out_size 512 \
+    -image_size 192 \
+    -out_size 192 \
     -dataset neurosam \
     -sam_ckpt "checkpoints/MedSAM2_pretrain.pth" \
-    -video_length 30 \
+    -video_length 16 \
     -b 2 \
     -data_path "/home/arinaldi/projects/rrg-mgoubran/arinaldi/SliceData" \
-    -sam_config sam2_hiera_t
+    -sam_config sam2_hiera_t_smaller_image
